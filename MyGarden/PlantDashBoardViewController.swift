788cea3a26740c6a69cc8562392f350a3e0b15c4
Mohanadevi <mohanadevik15@gmail.com>
2018-04-02 09:16:53 +0530

    project setup

---
+++ MyGarden/PlantDashBoardViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyGarden
+//
+//  Created by Devi on 02/04/18.
+//  Copyright © 2018 Devi. All rights reserved.
+//
+
+import UIKit
+
+class PlantDashBoardViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
